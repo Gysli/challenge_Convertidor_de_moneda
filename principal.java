a9689c7ebc40c401bf3ec417ee1a2f51bf0ccb9c
Gysli <156789288+Gysli@users.noreply.github.com>
2024-05-07 21:46:00 -0500

    Update principal.java

--- principal.java
+++ principal.java
@@ -1,102 +1,85 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class principal {
-    public static void main(String[] args) throws IOException {
+public class MenuPrinciapal {
 
-
-        double cantidadIngresadaUsuario = 0;
-        double resultadoConversion = 0;
-        int opcion = 0;
-
-
-        String busqueda2 = "ARS";
-        String busqueda1 = "USD";
-
-        Scanner scanner = new Scanner(System.in);
-        PrincipalAPI convertidor = new PrincipalAPI();
+    public static void main(String[] args) throws InterruptedException, IOException {
+        int opcion=0;
+        double CantidadIngresadaUsuaio;
+        double resultadoCambio;
+        ConversorApi consultaTasa  = new ConversorApi();
 
         String menu = """
-                *****************************************
-                Bienvenidos Al  Conversor de Monedas Alura
                 ++++++++++++++++++++++++++++++++++++++++++
-                                
-                1 - Dólar =>>>> Peso Argentino
-                2 - Peso Argentino =>>>> Dólar
-                3 - Dólar =>>>> Real Brasileño
-                4 - Real Brasileño =>>>>  Dólar
-                5 - Dólar =>>>>  Peso Colombiano
-                6 - Peso Colombiano =>>>> Dólar
-                7 - Salir
-                Elija una opción válida:
-                                
-                 ++++++++++++++++++++++++++++++++++++++++++
+                BIENVENIDO AL CONVERTIDOR DE MONEDAS ALURA
+                ++++++++++++++++++++++++++++++++++++++++++
+                1) Dólar a Pesos Argentinos
+                2) Peso Argentino a Dolar
+                3) Dólar a Real Brasileño
+                4) Real Brasileño a Dolar
+                5) Dolar a peso Colombiano
+                6) Peso Colombiano a Dolar
+                7) Salir
+                ++++++++++++++++++++++++++++++++++++++++++
+                ++++++++++++++++++++++++++++++++++++++++++
                 """;
+
+
+
         Scanner teclado = new Scanner(System.in);
 
-        while (opcion != 7) {
+        while (opcion != 7){
             System.out.println(menu);
             opcion = teclado.nextInt();
 
+//Fin de la segunda parte montando el menú de oppression
 
-//ininio de menú de convesión de monedas
-
+            System.out.println("Favor Digitar el Valor a Convertir");
+            Scanner teclado1 = new Scanner(System.in);
+            CantidadIngresadaUsuaio = teclado1.nextDouble();
 
             switch (opcion) {
                 case 1:
-                    System.out.println("Ingresa el valor a convertir: ");
-                    Scanner teclado1 = new Scanner(System.in);
-                    cantidadIngresadaUsuario = teclado1.nextDouble();
-                    busqueda1 = "USD";
-                    busqueda2 = "ARS";
-                    String resultado = convertidor.PrincipalAPI(opcion, cantidadIngresadaUsuario);
-                    System.out.println("El valor de:" + cantidadIngresadaUsuario + busqueda1 + "   A Pesos Argentinos es "  + busqueda2);
-                    System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+                    System.out.println("Convirtiendo de Dólares a Pesos Argentinos...");
+                    consultaTasa.tasadeCambio(moneda.USD, moneda.ARS, CantidadIngresadaUsuaio);
                     break;
-
                 case 2:
-                    System.out.println("Ingresa el valor a convertir: ");
-                    Scanner teclado2 = new Scanner(System.in);
-                    cantidadIngresadaUsuario = teclado2.nextDouble();
-                    System.out.println("El valor de:" + cantidadIngresadaUsuario + "ARS" + "A Dólar es " + resultadoConversion + "USD");
+                    System.out.println("Convirtiendo de Pesos Argentinos a Pesos Dólares...");
+                    consultaTasa.tasadeCambio(moneda.ARS, moneda.USD, CantidadIngresadaUsuaio);
                     break;
-
                 case 3:
-                    System.out.println("Ingresa el valor a convertir: ");
-                    Scanner teclado3 = new Scanner(System.in);
-                    cantidadIngresadaUsuario = teclado3.nextDouble();
-                    System.out.println("El valor de:" + cantidadIngresadaUsuario + "USD" + "A Real Brasileño" + resultadoConversion + "BRL");
+                    System.out.println("Convirtiendo de Dólares a Reales Brasileños...");
+                    consultaTasa.tasadeCambio(moneda.USD, moneda.BRL, CantidadIngresadaUsuaio);
                     break;
                 case 4:
-                    System.out.println("Ingresa el valor a convertir: ");
-                    Scanner teclado4 = new Scanner(System.in);
-                    cantidadIngresadaUsuario = teclado4.nextDouble();
-                    System.out.println("El valor de:" + cantidadIngresadaUsuario + "BRL" + "A Dólar" + resultadoConversion + "USD");
+                    System.out.println("Convirtiendo Reales Brasileños a Dólares...");
+                    consultaTasa.tasadeCambio(moneda.BRL, moneda.USD, CantidadIngresadaUsuaio);
                     break;
-
                 case 5:
-                    System.out.println("Ingresa el valor a convertir: ");
-                    Scanner teclado5 = new Scanner(System.in);
-                    cantidadIngresadaUsuario = teclado5.nextDouble();
-                    System.out.println("El valor de:" + cantidadIngresadaUsuario + "USD" + "A Peso Colombiano " + resultadoConversion + "COP");
+                    System.out.println("Convirtiendo  Dólares a Pesos Colombianos...");
+                    consultaTasa.tasadeCambio(moneda.USD, moneda.COP, CantidadIngresadaUsuaio);
                     break;
-
                 case 6:
-                    System.out.println("Ingresa el valor a convertir: ");
-                    Scanner teclado6 = new Scanner(System.in);
-                    cantidadIngresadaUsuario = teclado6.nextDouble();
-                    System.out.println("El valor de:" + cantidadIngresadaUsuario + "COP" + "A Dolar" + resultadoConversion + "USD");
+                    System.out.println("Convirtiendo Pesos Colombianos a Dólares...");
+                    consultaTasa.tasadeCambio(moneda.COP, moneda.USD, CantidadIngresadaUsuaio);
+                    break;
+                case 7:
+                    System.out.println("Finalizando el programa");
+                    System.out.println("Feliz  día!");
                     break;
 
-                case 7:
-                    System.out.println("Saliendo de la aplicación...");
-                    System.out.println("Gracias por Utilizar Nuestros servicios, Feliz día!");
+                default:
+                    System.out.println("Opción inválida");
                     break;
             }
         }
 
-        //fin de opcion menú de conversión de moneda
+        //Fin de la tercera parte implementando los cálculos
     }
 }
+
+
+
+
 
 
